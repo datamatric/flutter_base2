a18c077c0b15deeccaedb154660dd1d9147e803d
datamatric <sha2nkg@gmail.com>
2019-05-08 02:07:51 +0530

    Initial commit

---
+++ lib/api/messaging.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+
+class Messaging {
+  static final Client client = Client();
+
+  // from 'https://console.firebase.google.com'
+  // --> project settings --> cloud messaging --> "Server key"
+  static const String serverKey =
+      'AAAAvj2-XKc:APA91bHwjsxrlw1HbnHtpAkVePM5VGDOfASnVmANVJ4KkaKpx2VzcnmDx0jUMOUUubSKPFvnhh6Xmg_Wdq8y8c8Ed3fAiJ9FSJXYP6T9YhIEkN4tmpqnN9eqZoTFe078G9vmDtrkcaFV';
+
+  static Future<Response> sendToAll({
+    @required String title,
+    @required String body,
+  }) =>
+      sendToTopic(title: title, body: body, topic: 'all');
+
+  static Future<Response> sendToTopic(
+      {@required String title,
+        @required String body,
+        @required String topic}) =>
+      sendTo(title: title, body: body, fcmToken: '/topics/$topic');
+
+  static Future<Response> sendTo({
+    @required String title,
+    @required String body,
+    @required String fcmToken,
+  }) =>
+      client.post(
+        'https://fcm.googleapis.com/fcm/send',
+        body: json.encode({
+          'notification': {'body': '$body', 'title': '$title'},
+          'priority': 'high',
+          'data': {
+            'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+            'id': '1',
+            'status': 'done',
+          },
+          'to': '$fcmToken',
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'key=$serverKey',
+        },
+      );
+}
